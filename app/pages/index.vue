637af9a4304035b254d8bbe0798f4cf9e87c7406
Fustigate8933 <ianchang891@gmail.com>
2024-12-03 20:33:18 -0500

    embedded player setup.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,56 +1,52 @@
 <template>
-	<div class="flex items-center flex-col justify-center h-full w-full gap-5 py-[8rem]">
-		<div class="flex gap-3 items-baseline border-white border-b">
-			<h1 class="text-5xl text-yellow-100">Insomnia</h1>
-			<h1 class="text-xl text-orange-200">Eve</h1>
-		</div>
-		<div class="flex flex-col text-[#F5F5F5] text-2xl overflow-y-auto gap-3">
-			<p v-for="(lyric_line, i) in lyrics" :key="i">
-				{{ lyric_line }}	
-			</p>
+	<div class="flex items-center flex-col justify-center h-full w-full gap-5">
+		<div class="max-w-3xl flex items-center flex-col justify-center h-full w-full gap-5 py-[8rem]">
+			<div class="flex gap-3 items-baseline border-white border-b">
+				<h1 class="text-5xl text-yellow-100">{{song_name}}</h1>
+				<h1 class="text-xl text-orange-200">({{artist_name}})</h1>
+			</div>
+			<div class="flex flex-col text-[#F5F5F5] text-2xl overflow-y-auto gap-3 w-full">
+				<p v-for="(lyric_line, i) in lyrics" :key="i">
+					{{ lyric_line }}	
+				</p>
+			</div>
+			<h1>{{ embedUrl }}</h1>
+			<div class="w-full">
+				<div v-html="embedCode"></div>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
 const lyrics = ref([])
+const embedCode = ref("")
 
 const song_name = "Insomnia"
 const artist_name = "Eve"
 
 const fetchMusicData = async () => {
 	// const res = await fetch("/api/musicapi")
-	const searchResponse = await fetch("https://api.musicapi.com/public/search", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-			"Accept": "application/json",
-			"Authorization": "Token 2cf82db3-4064-4235-8253-16994eb51773"
-		},
-		body: {
-			type: "track",
-			track: "宇宙の季節",
-			sources: ["spotify"]
-		}
-	})
-	
-	const searchData = await searchResponse.json()
-	console.log(searchData)
+	const embeddingResponse = await fetch("/api/musicapi")
+	const embeddingResponseData = await embeddingResponse.json()
+	embedCode.value = embeddingResponseData.embedCode
 
-  const lyricsResponse = await fetch(`https://lrclib.net/api/search?track_name=${song_name}&artist_name=${artist_name}`)
+	const lyricsResponse = await fetch(`https://lrclib.net/api/search?track_name=${song_name}&artist_name=${artist_name}`)
 
-  if (!lyricsResponse.ok) {
-    lyrics.value = [`No song with name ${song_name} and artist ${artist_name} could be found.`]
-    throw new Error(`No song with name ${song_name} and artist ${artist_name} was found.`)
-  }
+	if (!lyricsResponse.ok) {
+		lyrics.value = [`No song with name ${song_name} and artist ${artist_name} could be found.`]
+		throw new Error(`No song with name ${song_name} and artist ${artist_name} was found.`)
+	}
 
-  const lyricsData = await lyricsResponse.json()
-  if (lyricsData.length === 0) {
-    lyrics.value = [`No song with name ${song_name} and artist ${artist_name} could be found.`]
-  }
+	const lyricsData = await lyricsResponse.json()
+	if (lyricsData.length === 0) {
+		lyrics.value = [`No song with name ${song_name} and artist ${artist_name} could be found.`]
+	}
 
-  const most_related = lyricsData[0]
-  lyrics.value = most_related.plainLyrics.split("\n")
+	const most_related = lyricsData[0]
+	lyrics.value = most_related.plainLyrics.split("\n")
+
+	console.log(most_related.syncedLyrics)
 }
 
 onMounted(() => {
--- server/api/musicapi.ts
+++ server/api/musicapi.ts
@@ -1,5 +1,42 @@
 export default defineEventHandler(async (event) => {
-	console.log(process.env.CLIENT_ID)
-	event.node.res.end()
+	try {
+		const searchResponse = await fetch("https://api.musicapi.com/public/search", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"Accept": "application/json",
+				"Authorization": `Token ${process.env.CLIENT_ID}`
+			},
+			body: JSON.stringify({
+				type: "track",
+				track: "宇宙の季節",
+				sources: ["spotify"]
+			}),
+			redirect: "follow"
+		})
+		
+		let searchData = await searchResponse.json()
+		searchData = searchData.tracks[0].data
+		const trackUrl = searchData.url
+		
+		const track = await fetch("https://api.musicapi.com/public/embed/url", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"Accept": "application/json",
+				"Authorization": `Token ${process.env.CLIENT_ID}`
+			},
+			body: JSON.stringify({
+				url: trackUrl
+			})
+		})
+
+		const trackData = await track.json()
+		// console.log(trackData)
+
+		return trackData
+	} catch (error) {
+		return error
+	}
 })
 
