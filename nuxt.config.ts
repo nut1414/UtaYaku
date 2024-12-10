edf7b16e946d1a646da56170b01f89015880e525
Fustigate8933 <ianchang891@gmail.com>
2024-12-09 22:40:00 -0500

    enabled database feature for nitro database

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	nitro:{
+		experimental: {
+			database: true	
+		}	
+	},
+
 	compatibilityDate: '2024-04-03',
 	devtools: { enabled: true },
 
