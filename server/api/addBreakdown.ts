23d4cccc2119a36a4274abfd12aca11d2e2cff50
Fustigate8933 <ianchang891@gmail.com>
2024-12-10 18:30:52 -0500

    table structure established

--- server/api/addBreakdown.ts
+++ server/api/addBreakdown.ts
@@ -2,17 +2,27 @@ export default defineEventHandler(async (event) => {
 	const { allBreakdowns, musicId } = await readBody(event)
 
 	const db = useDatabase("breakdowns")
-	console.log(db)
 
-	await db.sql`CREATE TABLE IF NOT EXISTS users ("id" TEXT PRIMARY KEY, "firstName" TEXT, "lastName" TEXT, "email" TEXT)`;
+	await db.sql`
+		CREATE TABLE IF NOT EXISTS Song (
+		song_id INTEGER PRIMARY KEY
+	)`
+
+	await db.sql`
+		CREATE TABLE IF NOT EXISTS Breakdowns (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		song_id INTEGER NOT NULL,
+		json_data TEXT NOT NULL,
+		FOREIGN KEY (song_id) REFERENCES Song(song_id)
+	)`
+
 	const tables = await db.sql`
 		SELECT name
-		FROM sqlite_master 
+		FROM sqlite_master
 		WHERE type = 'table'
 	`
 
-	console.log("Tables:", tables);
-
+	console.log(tables)
 
 	return allBreakdowns
 })
